28602cb498f4fc877740bde96263f1e05211eae3
thihoangmin <hoangminhteky123@gmail.com>
2025-06-07 17:16:57 +0700

    actor

---
+++ Assets/Scripts/ActorSOs/ActorSO.cs
@@ -0,0 +1,11 @@
+
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ActorSO", menuName = "Dialogue/NPC")]
+public class ActorSO : ScriptableObject
+{
+
+    public string actorName;
+    public Sprite portrait;
+
+}
